5494302dcf713a82b819ec0e5786a95bd98c3ec7
Phoenix Shane <phoenix.oconnell@gmail.com>
2020-07-22 00:48:32 -0500

    Steps 9 through 13 Exercise 1

--- tekcamp.sql
+++ tekcamp.sql
@@ -133,3 +133,74 @@ WHERE education = 'Associates Degree';
 SELECT * FROM tekcampers
 WHERE education = 'High School';
 
+CREATE TABLE ta_tim_phillips (
+id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+first_name VARCHAR(100),
+last_name VARCHAR(100)
+);
+
+INSERT INTO ta_tim_phillips (first_name, last_name)
+VALUES ('Gabe', 'Chavez'),
+('Abigail', 'Swigert'),
+('Adam', 'Rice'),
+('Marcelo', 'Barbosa'),
+('Julian', 'Ladd'),
+('Jose', 'Moreno'),
+('Phoenix', 'Shane'),
+('Michael', 'Kinney'),
+('Christian', 'Gonzalez'),
+('Monica', 'Howard'),
+('Devon', 'Brewster');
+
+CREATE TABLE hobbies (
+id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+first_name VARCHAR(100),
+last_name VARCHAR(100),
+hobby VARCHAR(255)
+);
+
+INSERT INTO hobbies (first_name, last_name, hobby)
+VALUES ('Gabe', 'Chavez', 'Cattle rustling'),
+('Abigail', 'Swigert', 'Reading'),
+('Adam', 'Rice', 'Golfing'),
+('Marcelo', 'Barbosa', 'Jiu Jitsu'),
+('Julian', 'Ladd', 'Pickle ball'),
+('Jose', 'Moreno', 'Soccer'),
+('Phoenix', 'Shane', 'Aerial yoga'),
+('Michael', 'Kinney', NULL),
+('Christian', 'Gonzalez', NULL),
+('Monica', 'Howard', NULL),
+('Devon', 'Brewster', NULL),
+('Aaron', 'White', NULL),
+('Caleb', 'Waters', 'Eating'),
+('Cody', 'Clark', 'Cooking'),
+('George', 'Torres', 'Gaming'),
+('Gowtham', 'Katta', 'Running'),
+('Jessica', 'Ulysse', 'Listening to music and creating playlists'),
+('John', 'Bosarov', 'Soccer'),
+('John', 'Kol', NULL),
+('Justin', 'Cheng', 'Eating'),
+('Katherine', 'Kim', 'Writing'),
+('Kevin', 'Ibanez', 'Working out'),
+('Kevin', 'Keesee', 'Video games'),
+('Korey', 'Brooks', NULL),
+('Matthew', 'Castiglione', 'Video games'),
+('Matthew', 'Choat', 'Longboarding'),
+('Nancy', 'Golden', NULL),
+('Norita', 'Sieffert', 'Motorcycle riding'),
+('Sabitha', 'Radhakrishnan', 'Doing engineering projects with kids'),
+('Sarah', 'Bates', NULL),
+('Tyler', 'Clements', 'Growing hot peppers'),
+('Vani', 'Muppuru', NULL),
+('Victor', 'Betts', 'Reading'),
+('Vimala', 'Murthy', 'Gardening'),
+('Zachary', 'Johnson', 'Hockey');
+
+SELECT ta_tim_phillips.first_name, ta_tim_phillips.last_name, hobbies.hobby FROM ta_tim_phillips
+LEFT JOIN hobbies ON ta_tim_phillips.last_name = hobbies.last_name;
+
+ALTER TABLE tekcampers
+ADD COLUMN bootcamp VARCHAR(255) AFTER education;
+
+ALTER TABLE ta_tim_phillips
+ADD COLUMN bootcamp VARCHAR(255) AFTER last_name;
